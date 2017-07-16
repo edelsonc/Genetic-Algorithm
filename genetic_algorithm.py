20ef22aee6a96b8d685eaee7cadba5d29154bee5
edelsonc <charles.edelson12@ncf.edu>
2017-07-16 14:17:31 -0700

    Separated the genetic algorithm and the example

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 """
-A simple application of a genetic algorithm to a well known classification
-problem -- the student exam problem
+A simple genetic algorithm class meant to solve general optimization problems
 
 author: edelsonc
 date: 7/14/2017
@@ -120,59 +119,3 @@ class Population(object):
     def best_fit(self):
         idx_best = sorted(self.fitness)[0][1]
         return self.individuals[idx_best]
-
-
-
-
-
-
-#-------------------------------Student Problem--------------------------------#
-def logistic_function(beta, h):
-    # logistic function for logistic regression
-    return 1/(1 + math.exp(-(beta[0] + beta[1]*h)))
-
-
-def loss_function(beta, hour, passed):
-    # square error loss function; used to measure fitness
-    sse = 0
-    for h,p in zip(hour,passed):
-        sse += (p - logistic_function(beta, h))**2
-    return sse
-
-
-def generate_individual(a, b, n):
-    # generates a list of n random parameters between a and b
-    return [random.uniform(a,b) for i in range(n)]
-
-def mutate(a,b):
-    # how a mutation on a parameter can be definied
-    return random.uniform(a,b)
-
-#------------------------------------Data--------------------------------------#
-hours = [ 0.50, 0.75, 1.00, 1.25, 1.50, 1.75, 1.75, 2.00, 2.25, 2.50,
-        2.75, 3.00, 3.25, 3.50, 4.00, 4.25, 4.50, 4.75, 5.00, 5.50 ]
-
-passed = [ 0, 0, 0, 0, 0, 0, 1, 0, 1, 0,
-        1, 0, 1, 0, 1, 1, 1, 1, 1, 1]
-
-if __name__ == "__main__":
-    # initialize parameters
-    p_size = 40
-    param_bounds = [-10, 10]
-    n_params = 2
-    per_p = 0.3
-    per_k = 0.5
-    per_s = 0.1
-    p_mute = 0.05
-    hours_args = [hours for i in range(p_size)]
-    passed_args = [passed for i in range(p_size)]
-
-    # begin optimizations
-    p = Population(p_size, loss_function, generate_individual, mutate)
-    p.generate_population(*param_bounds, n_params)
-    for i in range(1000):
-        p.eval_pop_fitness(hours_args, passed_args)
-        p.next_gen(per_p, per_s, per_k, *param_bounds, n_params)
-        p.mutate(p_mute,*param_bounds)
-        print("Average fitness: ", p.average_fitness())
-    print("Final Parameters: ", p.best_fit())
